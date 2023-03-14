49024d667614618c0590cfa622b62f4894f1a304
Robert Gorz <rjgorz@gmail.com>
2023-03-13 21:46:17 -0500

    completed

--- server/app.py
+++ server/app.py
@@ -19,15 +19,61 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    response_body = f'''
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure.environment}</ul>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    response_body = f'''
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+    '''
+    animals = [animal for animal in zookeeper.animals]
+
+    if not animals:
+        response_body += '<ul>This keeper has no animals.</ul>'
+    else:
+        for animal in animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+
+    response = make_response(response_body, 200)
+
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+
+    response_body = f'''
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+    '''
+    animals = [animal for animal in enclosure.animals]
+
+    if not animals:
+        response_body += '<ul>This enclosure has no animals.</ul>'
+    else:
+        for animal in animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+
+    response = make_response(response_body, 200)
+
+    return response
 
 
 if __name__ == '__main__':
